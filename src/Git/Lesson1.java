2001f5e6a742dc8db4cfd8adc684becbc932231b
ComradeAlex97 <alexeirusnac1997@gmail.com>
2022-10-19 12:22:54 +0300

    Выполнил первое дз

---
+++ src/Git/Lesson1.java
@@ -0,0 +1,10 @@
+package Git;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        print();
+    }
+    private static void print(){
+        System.out.println("1st lesson");
+    }
+}
